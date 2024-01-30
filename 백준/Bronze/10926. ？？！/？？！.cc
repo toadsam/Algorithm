57363e7bf7f28289c4b0f1362adb187aaccc1808
toadsam <75034052+toadsam@users.noreply.github.com>
2024-01-30 19:34:37 +0900

    [Bronze V] Title: ??!, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    string id;
+    cin >> id;
+    
+    id += "??!";
+    cout << id;
+    
+    return 0;
+    
+}
