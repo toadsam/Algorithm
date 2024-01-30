6e6067c289925831d8fbcf67f2c2fadb0b722e70
toadsam <75034052+toadsam@users.noreply.github.com>
2024-01-30 19:26:45 +0900

    [Bronze V] Title: 사칙연산, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[]){
+    int a , b;
+    
+    cin >> a >> b; //입력받기
+    
+    cout << a + b << "\n"; //덧셈
+    cout << a - b << "\n";
+    cout << a * b << "\n";
+    cout << a/b << "\n";
+    cout << a % b << "\n";
+    return 0;
+}
