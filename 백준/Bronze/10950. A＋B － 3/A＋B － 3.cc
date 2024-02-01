490f97f14adcbafff208bb69920d957d1b99cc79
toadsam <75034052+toadsam@users.noreply.github.com>
2024-02-01 18:20:25 +0900

    [Bronze V] Title: A+B - 3, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv){
+    int T,A,B;
+    
+    cin >> T;
+    
+    for(int i = 0 ; i < T; i++){
+        cin >> A >> B ;
+        cout << A + B << "\n";
+    }
+    return 0;
+}
