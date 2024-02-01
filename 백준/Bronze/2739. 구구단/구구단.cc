e11bec9cd9dd670084918e8e6797a33e31ee5d7b
toadsam <75034052+toadsam@users.noreply.github.com>
2024-02-01 18:13:56 +0900

    [Bronze V] Title: 구구단, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[]){
+    int N;
+    cin >> N;
+    
+    for(int i = 1; i < 10; i ++){
+        cout << N << " * " << i << " = " << N * i << "\n";
+    }
+    return 0;
+}
