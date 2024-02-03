f71a9c8ee5fc90641401f4b797de7ab9100c99bc
toadsam <75034052+toadsam@users.noreply.github.com>
2024-02-03 17:06:50 +0900

    [Bronze III] Title: 최댓값, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2562. 최댓값/최댓값.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, const char *argv[]){
+    
+    int num[9];
+    for(int i = 0; i < 9; i++){
+        cin >> num[i];
+    }
+    
+    int maxValue = -1;
+    int maxValueIndex;
+    for (int i = 0 ; i < 9; i++){
+        int maxValue = -1;
+        int maxValueIndex;
+        for(int i = 0; i < 9; i++){
+            if(num[i] > maxValue){
+                maxValue = num[i];
+                maxValueIndex = i;
+            }
+        }
+        cout << maxValue << "\n";
+        cout << maxValueIndex + 1;
+        return 0;
+        
+}
+}
