f352d69075e2ddef4846828185c05b57804bda6c
toadsam <75034052+toadsam@users.noreply.github.com>
2024-02-03 14:58:39 +0900

    [Bronze V] Title: X보다 작은 수, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[]){
+    int N,X;
+    cin >> N >> X;
+    
+    int arr[10000];
+    
+    for(int i = 0; i < N; i++){
+        cin >> arr[i];
+    }
+    
+    for(int i = 0; i < N;  i++){
+        if(arr[i] < X){
+            cout << arr[i] << " ";
+}
+}
+    return 0;
+}
