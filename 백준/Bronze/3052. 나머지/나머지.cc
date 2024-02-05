15cc46eccf3192e5ee30cb0f84d6f156dcff07c8
toadsam <75034052+toadsam@users.noreply.github.com>
2024-02-05 11:24:28 +0900

    [Bronze II] Title: 나머지, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3052. 나머지/나머지.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, const char * argv[]){
+    
+    int count[42] = {};
+    
+    int v;
+    for(int i =0; i < 10; i ++){
+        cin >> v;
+        count[v%42]++;
+    }
+    
+    int result = 0;
+    
+    for(int v : count){
+        if(v > 0){
+            result++;
+        }
+    }
+    cout << result;
+    return 0;
+    
+    
+}
