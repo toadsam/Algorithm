3cc78777c3a93b43ccaf5cee2a89be5d46e2a263
toadsam <75034052+toadsam@users.noreply.github.com>
2024-04-10 22:31:26 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 4, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24265. 알고리즘 수업 － 알고리즘의 수행 시간 4/알고리즘 수업 － 알고리즘의 수행 시간 4.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+ 
+    long n;
+    cin >> n; 
+ 
+    cout << (n*(n-1))/2 << '\n' << 2 << endl; 
+ 
+    return 0; //너무 어려워서 보고했습니다....ㅠㅠ
+}
