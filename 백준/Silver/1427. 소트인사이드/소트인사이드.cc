b73bc24e39ecccdd2d5480ae8f73108b2e16bcf9
toadsam <75034052+toadsam@users.noreply.github.com>
2024-05-06 14:46:02 +0900

    [Silver V] Title: 소트인사이드, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string n;
+    cin >> n;  
+
+    
+    sort(n.begin(), n.end(), greater<char>());
+
+    cout << n;  
+
+    return 0;
+}
