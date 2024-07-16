7ed36e77422a732d45eca6851a7c81ad35d614c3
toadsam <75034052+toadsam@users.noreply.github.com>
2024-07-16 10:16:54 +0900

    [Bronze II] Title: Hashing, Time: 0 ms, Memory: 2024 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.cc
+++ 백준/Bronze/15829. Hashing/Hashing.cc
@@ -1,23 +1,24 @@
-#include<iostream>
-#include<string>
-#include<cmath>
+#include <iostream>
+#include <string>
 using namespace std;
 
-int main()
-{
+int main() {
     int N;
-    long sum = 0;
+    long long sum = 0;
     string str;
-    
+    const int M = 1234567891; // 모듈러 연산에 사용할 값
+    const int r = 31; // 주어진 상수 값
+
     cin >> N;
     cin >> str;
-    for(int i = 0 ; i < str.length() ; i++)
-    {
-        int value = str[i] - 'a' + 1;
-        sum += pow(31,i)*value;
-        
+
+    long long r_power = 1; // r의 거듭제곱을 저장할 변수
+    for (int i = 0; i < N; i++) {
+        int value = str[i] - 'a' + 1; // 각 문자의 값을 계산
+        sum = (sum + value * r_power) % M; // 현재 합에 더하고 모듈러 연산
+        r_power = (r_power * r) % M; // r의 다음 거듭제곱을 계산하고 모듈러 연산
     }
-    
-    cout << sum << '\n';
+
+    cout << sum << '\n'; // 최종 해시 값을 출력
     return 0;
 }
