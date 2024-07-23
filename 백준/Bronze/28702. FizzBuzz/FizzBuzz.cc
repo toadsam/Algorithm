0876882133713479d8b449309d9095b990f70135
toadsam <75034052+toadsam@users.noreply.github.com>
2024-07-24 01:09:10 +0900

    [Bronze I] Title: FizzBuzz, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/28702. FizzBuzz/FizzBuzz.cc
@@ -0,0 +1,46 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <cmath>
+#include <cstring>
+
+using namespace std;
+
+int main() {
+	cin.tie(0);
+	cout.tie(0);
+	string a, b, c;
+	cin >> a >> b >> c;
+	if(a[a.size() - 1] != 'z'){
+		int next = stoi(a) + 3;
+		if(next % 3== 0 && next % 5 == 0){
+			cout << "FizzBuzz";
+		}
+		else if(next % 3 == 0) cout << "Fizz";
+		else if(next % 5 == 0) cout << "Buzz";
+		else cout << next;
+	}
+	else if(b[b.size() - 1] != 'z'){
+		int next = stoi(b) + 2;
+		if(next % 3== 0 && next % 5 == 0){
+			cout << "FizzBuzz";
+		}
+		else if(next % 3 == 0) cout << "Fizz";
+		else if(next % 5 == 0) cout << "Buzz";
+		else cout << next;
+	}
+	else{
+		int next = stoi(c) + 1;
+		if(next % 3== 0 && next % 5 == 0){
+			cout << "FizzBuzz";
+		}
+		else if(next % 3 == 0) cout << "Fizz";
+		else if(next % 5 == 0) cout << "Buzz";
+		else cout << next;
+	}
+	return 0;
+}
+//혼자 못풀겠어
