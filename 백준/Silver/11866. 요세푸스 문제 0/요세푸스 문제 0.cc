b19b77f4f58fe6bc9b393b41c515971b9b08bea2
toadsam <75034052+toadsam@users.noreply.github.com>
2024-07-29 13:28:42 +0900

    [Silver V] Title: 요세푸스 문제 0, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main() {
+    int N, K;
+    cin >> N >> K;
+    queue<int> q;
+    
+    
+    for (int i = 1; i <= N; i++) {
+        q.push(i);
+    }
+
+    cout << "<";
+    while (!q.empty()) {
+        
+        for (int i = 0; i < K - 1; i++) {
+            q.push(q.front());
+            q.pop();
+        }
+        
+        
+        cout << q.front();
+        q.pop();
+        if (!q.empty()) {
+            cout << ", ";
+        }
+    }
+    cout << ">" << endl;
+
+    return 0;
+}
