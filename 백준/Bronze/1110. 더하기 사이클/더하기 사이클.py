7dc22a7e65155432e621fd187ecc6bcef59a772f
toadsam <75034052+toadsam@users.noreply.github.com>
2025-05-23 12:24:32 +0900

    [Bronze I] Title: 더하기 사이클, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/1110. 더하기 사이클/더하기 사이클.py
+++ 백준/Bronze/1110. 더하기 사이클/더하기 사이클.py
@@ -1,13 +1,14 @@
 n = int(input())
+
 original = n
 count = 0
 
 while True:
     a = n//10
     b = n%10
-    s = a+b
-    n = (b*10)+(s%10)
-    count += 1
-    if n == original:
+    c = a + b
+    n = (b*10) + (c%10)
+    count +=1
+    if original == n:
         break
 print(count)
