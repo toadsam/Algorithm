b42f79883b89cba837372c6dd4c574dfaf7bc278
toadsam <75034052+toadsam@users.noreply.github.com>
2025-05-23 12:37:00 +0900

    [Bronze I] Title: 평균, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/1546. 평균/평균.py
@@ -0,0 +1,8 @@
+n = int(input())
+scores = list(map(int, input().split()))
+
+max_score = max(scores)
+new_scores = [(score / max_score) * 100 for score in scores]
+average = sum(new_scores) / n
+
+print(average)
